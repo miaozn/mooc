fd4f383b08af7a18b4abe4b8683f814022fca9c5
zm <zngmiao@gmail.com>
2015-06-06 14:32:14 -0700

    3-1 done

--- thu-ds/pa3-1.cpp
+++ thu-ds/pa3-1.cpp
@@ -25,29 +25,27 @@ struct fastio{
 
 
 
+char edge[10001*10001] = {0};
 
 int main(){
 	int n, m;
 	scanf("%d", &n);
-	char edge[n+1][n+1];
-	memset(edge, 0, (n+1)*(n+1));
-
 	scanf("%d", &m);
 
 	int e1, e2;
 	while(m--){
 		scanf("%d", &e1);
 		scanf("%d", &e2);
-		edge[e1][e2] = 1;
-		edge[e2][e1] = 1;
+		edge[e1*(n+1)+e2] = 1;
+		edge[e2*(n+1)+e1] = 1;
 	}
 
 	int i,j,k;
 	for(i=1;i<n+1;i++){
 		for(j=i+1;j<n+1;j++){
-			if(edge[i][j]){
+			if(edge[i*(n+1)+j]){
 				for(k=j+1;k<n+1;k++){
-					if(edge[i][k] && edge[j][k]){
+					if(edge[i*(n+1)+k] && edge[j*(n+1)+k]){
 						printf("-1\n");
 						return 0;
 					}
